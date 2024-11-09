b6473c2ffc8e2798c6a0c6324ab5b56a290a2bfc
Oussama Elhousni <oussamaelhousnioe@gmail.com>
2024-11-09 15:19:34 +0100

    adds user model

---
+++ models/userModel.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+const uuid = require("uuid");
+
+const userSchema = new mongoose.Schema({
+  fullName: {
+    type: String,
+    require: [true],
+  },
+  email: {
+    type: String,
+    unique: true,
+    validate: [validator.isEmail, "Please provide a valid email"],
+  },
+  confirmationId: {
+    type: String,
+    default: uuid.v4,
+  },
+  isConfirmed: {
+    type: Boolean,
+    default: "false",
+  },
+  otp: String,
+  isDeleted: {
+    type: Boolean,
+    default: false,
+  },
+
+  lastLoginAt: Date,
+});
+
+const userModel = mongoose.model("User", userSchema);
+module.exports = userModel;
